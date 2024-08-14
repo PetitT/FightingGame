d0d0eef1133ba54b874d26eb5adf3e97208e890d
PetitT <xcthib@hotmail.com>
2024-08-14 19:21:09 +0200

    Add basic animations

---
+++ FightingGame/Assets/Scripts/Character/CharacterAnimation/CharacterAnimation.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+
+}
+
+[Serializable]
+public class AnimationFrame
+{
+    [SerializeField] private GameObject _parent;
+}
